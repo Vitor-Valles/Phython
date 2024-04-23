1e2f5dcd585bc30ded6a496fa7791ecf0b7cadda
Vitor Andrade Valles <161478753+Vitor-Valles@users.noreply.github.com>
2024-04-23 16:59:30 -0300

    Add files via upload

---
+++ aula-01.py
@@ -0,0 +1,27 @@
+#Programa aula 1
+#Autor: Vitor Andrade
+#Licença: GNU
+#Data 24/04/23
+
+#Criar um variavel com o nome e o sobrenome do usúario
+#Nas variaveis nome e sobreNome Captura via atribuição os dados do usuário
+nome = "Vitor"
+sobreNome ="Andrade"
+#Na variavel nomeCompleto concatena o nome e o sobrenome
+#Simbulo de + e concatenação tem um exemplo abaixo 
+nomeCompleto = nome + " " + sobreNome
+#Imprima em tala a variavel nomeCompleto
+print(nomeCompleto)
+print(nome,"",sobreNome)
+#Variavel numerica do tipo inteiro
+numero = 5
+numero = 5.6
+#Variavel declado com o tipo inteiro e flutuante
+numero1 = int(10)
+numero2 = float(9.32)
+#string recebe qualquer caracter alpha numerico 
+#Quando um esta dentro das aspas ele e lido como texto 
+# aluno = str("8")
+aluno = str("vitor")
+#f <-- linha de saida
+print(f'nome do aluno é {nome}. texto com numero {8}')
